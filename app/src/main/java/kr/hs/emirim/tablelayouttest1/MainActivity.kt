822327f2f8db40eeb7e1031ec729991c5852bb96
huihyeon06 <agnea0829@gmail.com>
2023-06-15 18:56:26 +0900

    GridLayout 직접풀어보기 5-5 완성

--- app/src/main/java/kr/hs/emirim/tablelayouttest1/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/tablelayouttest1/MainActivity.kt
@@ -2,9 +2,11 @@ package kr.hs.emirim.tablelayouttest1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var edit1 : EditText
@@ -21,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     var btnIds = arrayOf(R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9, R.id.btn10)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.ex5_2)
+        setContentView(R.layout.dir5_5)
         edit1 = findViewById(R.id.edit1)
         edit2 = findViewById(R.id.edit2)
         btnPlus = findViewById(R.id.btnPlus)
@@ -29,5 +31,40 @@ class MainActivity : AppCompatActivity() {
         btnMultiply = findViewById(R.id.btnMultiply)
         btnDivide = findViewById(R.id.btnDivide)
         textResult = findViewById(R.id.textResult)
+
+        btnPlus.setOnClickListener(btnListener)
+        btnMinus.setOnClickListener(btnListener)
+        btnMultiply.setOnClickListener(btnListener)
+        btnDivide.setOnClickListener(btnListener)
+
+        for(i in 0..btnIds.size-1 step 1){
+            btns.add(findViewById(btnIds[i]))
+        }
+        for(i in 0..btnIds.size-1 step 1){
+            btns[i].setOnClickListener{
+                if(edit1.isFocused){
+                    num1 = edit1.text.toString()+btns[i].text.toString()
+                    edit1.setText(num1)
+                }else if(edit1.isFocused){
+                    num2 = edit2.text.toString()+btns[i].text.toString()
+                    edit2.setText(num2)
+                }else {
+                    Toast.makeText(applicationContext, "먼저 입력 할 곳에 포커스를 주세요", Toast.LENGTH.SHORT).show()
+                }
+            }
+        }
+        var btnListener=OnClickListener{
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+
+            when(it.id){
+                R.id.btnPlus->result = num1.toInt()+num2.toInt()
+                R.id.btnMinus->result = num1.toInt()-num2.toInt()
+                R.id.btnMultiply->result = num1.toInt()*num2.toInt()
+                R.id.btnDivide->result = num1.toInt()/num2.toInt()
+            }
+            textResult.setText(R.string.text_result)
+            textResult.append(result.toString())
+        }
     }
 }
